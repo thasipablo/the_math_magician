8edcc28f62eff6f22ab0fb44f2a3e41a0f63afd8
Thasi Pablo <39631185+thasipablo@users.noreply.github.com>
2023-08-15 22:32:03 +0100

    build the calculator ui

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ResultScreen from './ResultScreen';
+
+const Calculator = () => (
+  <div className="container">
+    <ResultScreen />
+    <div className="blocks">
+      <div className="block-item">AC</div>
+      <div className="block-item">+/-</div>
+      <div className="block-item">%</div>
+      <div className="block-item right-col">/</div>
+      <div className="block-item">7</div>
+      <div className="block-item">8</div>
+      <div className="block-item">9</div>
+      <div className="block-item right-col">x</div>
+      <div className="block-item">4</div>
+      <div className="block-item">5</div>
+      <div className="block-item">6</div>
+      <div className="block-item right-col">-</div>
+      <div className="block-item">1</div>
+      <div className="block-item">2</div>
+      <div className="block-item">3</div>
+      <div className="block-item right-col">+</div>
+      <div className="block-item zero">0</div>
+      <div className="block-item">.</div>
+      <div className="block-item right-col">=</div>
+    </div>
+  </div>
+);
+
+export default Calculator;
---
+++ src/components/ResultScreen.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const ResultScreen = () => <div className="block-item result">0</div>;
+
+export default ResultScreen;
