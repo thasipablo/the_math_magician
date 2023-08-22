fadf6260ee2aec105f3fe1814cc6f619f0c9e6a7
Thasi Pablo <39631185+thasipablo@users.noreply.github.com>
2023-08-23 00:01:46 +0100

    add quotes page style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Home = () => (
-  <>
+  <section className="home-page">
     <h1>Welcome to our page !</h1>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Id reiciendis
@@ -15,7 +15,7 @@ const Home = () => (
       numquam dolores, porro a vel quo amet fuga corporis maiores repellat
       deleniti ipsam placeat itaque ducimus pariatur non possimus? Blanditiis!
     </p>
-  </>
+  </section>
 );
 
 export default Home;
